f2b133b00b0bef5c3a85fa7dd8c3fe471d56cc56
sylentbv <one@nowhere.com>
2017-03-15 19:56:59 -0500

    working initial code

---
+++ src/com/bradly/Ticket.java
@@ -0,0 +1,46 @@
+package com.bradly;
+
+/**
+ * Created by sylentbv on 3/15/2017.
+ */
+
+import java.util.Date;
+
+public class Ticket {
+
+    private int priority;
+    private String reporter; //Stores person or department who reported issue
+    private String description;
+    private Date dateReported;
+
+    //STATIC Counter - accessible to all Ticket objects.
+    //If any Ticket object modifies this counter, all Ticket objects will have the modified value
+    //Make it private - only Ticket objects should have access
+    private static int staticTicketIDCounter = 1;
+
+    public int getTicketID() {
+        return ticketID;
+    }
+
+    //The ID for each ticket - instance variable. Each Ticket will have its own ticketID variable
+    protected int ticketID;
+
+    public Ticket(String desc, int p, String rep, Date date) {
+        this.description = desc;
+        this.priority = p;
+        this.reporter = rep;
+        this.dateReported = date;
+        this.ticketID = staticTicketIDCounter;
+        staticTicketIDCounter++;
+    }
+
+    protected int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString(){
+        return("ID: " + this.ticketID + " Issue: " + this.description + " Priority: " + 					this.priority + " Reported by: "
+                + this.reporter + " Reported on: " + this.dateReported);
+    }
+}
